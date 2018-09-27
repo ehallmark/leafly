47348fbb046c21bd028c5bff7b2110588b83bee4
ehallmark <ehallmark@gttgrp.com>
2018-09-27 09:54:06 -0700

    c

--- src/main/java/recommendation/Recommender.java
+++ src/main/java/recommendation/Recommender.java
@@ -8,9 +8,11 @@ public class Recommender {
 
 
     public static void main(String[] args) throws Exception {
+        // get categorical data
         SimilarityEngine effectSim = new SimilarityEngine(Database.loadEffects());
         SimilarityEngine flavorSim = new SimilarityEngine(Database.loadFlavors());
         SimilarityEngine parentSim = new SimilarityEngine(Database.loadParentStrains());
+        // get author rating strain triplets
 
     }
 }
