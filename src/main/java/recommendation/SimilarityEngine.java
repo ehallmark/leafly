d9478efb98cec023d36011eb64149128116caabf
ehallmark <ehallmark@gttgrp.com>
2018-09-28 23:23:02 -0700

    c

--- src/main/java/recommendation/SimilarityEngine.java
+++ src/main/java/recommendation/SimilarityEngine.java
@@ -7,13 +7,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class SimilarityEngine {
-    private Collection<String> labels;
     private Map<String, Integer> indexMap;
-    public SimilarityEngine(Collection<String> labels) {
-        this.labels = labels;
+    private double[][] featureSimMatrix;
+    
+    public SimilarityEngine(Collection<String> labels, double[][] featureSimMatrix) {
         AtomicInteger idx = new AtomicInteger(0);
-        indexMap = labels.stream().distinct()
+        this.indexMap = labels.stream().distinct()
                 .sorted().collect(Collectors.toMap(e->e,e->idx.getAndIncrement()));
+        this.featureSimMatrix = featureSimMatrix;
     }
 
     public double similarity(Collection<String> labels1, Collection<String> labels2) {
@@ -42,7 +43,11 @@ public class SimilarityEngine {
             }
         }
         if(found1 && found2) {
-            return cosineSimilarity(v1, v2);
+            if(featureSimMatrix!=null) {
+                return softCosineSimilarity(v1, v2, featureSimMatrix);
+            } else {
+                return cosineSimilarity(v1, v2);
+            }
         } else {
             return 0;
         }
@@ -64,7 +69,29 @@ public class SimilarityEngine {
         } else {
             return 0;
         }
-    };
+    }
+
+
+    private static double softCosineSimilarity(double[] v1, double[] v2, double[][] s) {
+        final int n = v1.length;
+        double ab = 0;
+        double a = 0;
+        double b = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++ ) {
+                double sij = s[i][j];
+                ab += v1[i] * v2[j] * sij;
+                a += v1[i] * v1[j] * sij;
+                b += v2[i] * v2[j] * sij;
+            }
+        }
+        if(a>0 && b > 0) {
+            return ab/(Math.sqrt(a)*Math.sqrt(b));
+        } else {
+            return 0;
+        }
+    }
+
 
     private static Map<String,Double> toMap(Collection<String> labels) {
         return labels.stream().distinct().collect(Collectors.toMap(l->l,l->1d));
